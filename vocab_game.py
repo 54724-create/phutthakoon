1bcdade3a24a0e5780b2f681183a22809c4e6d37
54724-create <54724@yupparaj.ac.th>
2026-08-17 13:38:32 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -129,5 +129,5 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวพุทธคุณ ขันธปราบ เลขที่ 32 ม.4/3")
 
